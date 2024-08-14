40df66dc98485809f0fa1a3a9d865952e5dc7c21
unknown <ajgarza27@gmail.com>
2024-08-14 18:51:38 -0500

    pushing last changes

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,30 +1,73 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+// webpack.config.js
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { GenerateSW } = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
-
-    module: {
-      rules: [
-        
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/js/index.js",
+    install: "./src/js/install.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // Ensure this path is correct
+      filename: "index.html",
+      chunks: ["main"],
+    }),
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: "UTA Text Editor",
+      short_name: "Text Editor",
+      description: "Save Note Here",
+      background_color: "#225ca3",
+      theme_color: "#225ca3",
+      start_url: "/",
+      publicPath: "/",
+      icons: [
+        {
+          src: path.resolve("src/images/logo.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          purpose: "any maskable",
+          destination: path.join("assets", "icons"),
+        },
       ],
+    }),
+    new GenerateSW({
+      clientsClaim: true,
+      skipWaiting: true,
+      swDest: "src-sw.js",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
     },
-  };
+    compress: true,
+    port: 3000,
+  },
 };
